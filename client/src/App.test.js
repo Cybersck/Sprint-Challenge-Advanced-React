47b2bdc13bea870bcb2ad3584e14a7af835380b6
kevin lautermilch <kevin.laut@outlook.com>
2020-03-13 12:29:09 -0400

    :)

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {render, mount, waitForElement, getByAltText, getAllByText} from '@testing-library/react'
+import "@testing-library/jest-dom/extend-expect";
 import App from './App';
+import GetData from './Components/GetData'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('see that data is returned from api', async () => {
+
+const {getAllByText} = render(<App/>);
+
+const data = await waitForElement(() => getAllByText(/Name:/i));
+
+expect(data[0]).toBeInTheDocument();
+
+})
